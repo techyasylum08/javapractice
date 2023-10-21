d57f870f2d03644a9ac17a481499e7057b77e986
techyasylum08 <techyasylum@gmail.com>
2023-10-21 07:54:40 +0530

    first commit java pgm

---
+++ Addition.java
@@ -0,0 +1,12 @@
+package practice_session;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a = 200, b = 30,c;
+		c = a + b;
+		System.out.println("c : "+c);
+
+	}
+
+}
---
+++ Welcome.java
@@ -0,0 +1,9 @@
+package practice_session;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
